ec82ba4b8c08ae08466884179f1c047ec7a1fa7b
JunAndrey <106904007+JunAndrey@users.noreply.github.com>
2022-09-18 19:47:35 +0300

    Update salary.py

--- salary.py
+++ salary.py
@@ -2,7 +2,3 @@ import datetime
 def calculate_salary():
     a = datetime.datetime.today().strftime("%d.%m.%Y")
     print(f' {a}  I like the salary!')
-
-
-if __name__ == '__main__':
-    calculate_salary()
