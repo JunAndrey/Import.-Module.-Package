dfa0f39db7402db61eee1aa24ec70e7964ece3fc
JunAndrey <106904007+JunAndrey@users.noreply.github.com>
2022-09-19 20:01:59 +0300

    Add files via upload

---
+++ accounting/application/db/people.py
@@ -0,0 +1,4 @@
+import datetime
+def get_employees():
+    a = datetime.datetime.now().strftime("%d.%m.%Y")
+    print(F' {a}  Hello, peoples!')
---
+++ accounting/application/salary.py
@@ -0,0 +1,4 @@
+import datetime
+def calculate_salary():
+    a = datetime.datetime.now().strftime("%d.%m.%Y")
+    print(f' {a}  I like the salary!')
